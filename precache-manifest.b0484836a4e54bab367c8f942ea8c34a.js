32b6c7effbb985b1947e1a8f27999a96b2946fe2
Iliya <zabaron@gmail.com>
2020-07-30 22:09:19 +0300

    Updates

---
+++ precache-manifest.b0484836a4e54bab367c8f942ea8c34a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d801fb8ab14e00bd381ea583df96a72d",
+    "url": "/stop-panic/index.html"
+  },
+  {
+    "revision": "ce77b00643bc2d28213d",
+    "url": "/stop-panic/static/css/main.12eab20d.chunk.css"
+  },
+  {
+    "revision": "97e9545635f8bc588c8b",
+    "url": "/stop-panic/static/js/2.4218ff92.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/stop-panic/static/js/2.4218ff92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce77b00643bc2d28213d",
+    "url": "/stop-panic/static/js/main.49b00da0.chunk.js"
+  },
+  {
+    "revision": "6bb5f692f2dcb195c80a",
+    "url": "/stop-panic/static/js/runtime-main.dbdc5aed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Iliya-hacker/stop-panic/precache-manifest.fb281e3dbc262253e95a80c05be62f8c.js"
+  "/stop-panic/precache-manifest.b0484836a4e54bab367c8f942ea8c34a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Iliya-hacker/stop-panic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stop-panic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
