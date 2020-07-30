1f9b3caf2e52115591985f756c21bef927ad6b15
Iliya <zabaron@gmail.com>
2020-07-30 20:48:14 +0300

    Updates

---
+++ precache-manifest.fb281e3dbc262253e95a80c05be62f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e10551613f0fccefdc25ca3f834c68",
+    "url": "/Iliya-hacker/stop-panic/index.html"
+  },
+  {
+    "revision": "726e7862e31fb2ea88b0",
+    "url": "/Iliya-hacker/stop-panic/static/css/main.12eab20d.chunk.css"
+  },
+  {
+    "revision": "97e9545635f8bc588c8b",
+    "url": "/Iliya-hacker/stop-panic/static/js/2.4218ff92.chunk.js"
+  },
+  {
+    "revision": "be6629bd7ab7e127b5d76ac590c4478e",
+    "url": "/Iliya-hacker/stop-panic/static/js/2.4218ff92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726e7862e31fb2ea88b0",
+    "url": "/Iliya-hacker/stop-panic/static/js/main.a9158510.chunk.js"
+  },
+  {
+    "revision": "ab7f4f1f8c88457d2cc5",
+    "url": "/Iliya-hacker/stop-panic/static/js/runtime-main.fcd1ad6b.js"
+  }
+]);
